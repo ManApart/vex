40f047ee3183ca5460ffb06e48bd528722fe983a
ManApart <r@austinkucera.com>
2021-07-23 18:49:39 -0400

    body based movement

--- src/commonMain/kotlin/ui/Player.kt
+++ src/commonMain/kotlin/ui/Player.kt
@@ -4,6 +4,7 @@ import clamp
 import com.soywiz.klock.TimeSpan
 import com.soywiz.klock.timesPerSecond
 import com.soywiz.korev.Key
+import com.soywiz.korge.box2d.body
 import com.soywiz.korge.box2d.registerBodyWithFixture
 import com.soywiz.korge.input.keys
 import com.soywiz.korge.view.*
@@ -11,16 +12,17 @@ import com.soywiz.korim.color.Colors
 import com.soywiz.korma.geom.Point
 import level.LevelMap
 import level.Tile
+import org.jbox2d.common.Vec2
 import org.jbox2d.dynamics.BodyType
 import ui.level.TILE_SIZE
 import kotlin.math.abs
 import kotlin.math.min
 
-const val MAX_X_VEL = 6.0
+const val MAX_X_VEL = 2f
 const val MAX_X_AIR_VEL = 6.0
 const val MAX_Y_VEL = 10.0
 const val GRAVITY = 20.0
-private const val ACCELERATION = .2
+private const val ACCELERATION = .2f
 private const val FRICTION = 1.0
 
 private const val JUMP_VELOCITY = 10
@@ -33,37 +35,33 @@ private const val DASH_VELOCITY = 20
 private const val DASH_TIME = .15
 
 class Player(private val map: LevelMap) : Container() {
-    var velocity = Point()
 
     fun init(spawnTile: Tile) {
-        solidRect(TILE_SIZE, 2 * TILE_SIZE, Colors.PINK)
+        solidRect(0.9 * TILE_SIZE, 1.5 * TILE_SIZE, Colors.PINK)
         position(spawnTile.x * TILE_SIZE, spawnTile.y * TILE_SIZE)
 
-        registerBodyWithFixture(type = BodyType.DYNAMIC, density = 2, friction = 0.01, gravityScale = 0.0)
-//            registerBodyWithFixture(type = BodyType.DYNAMIC, density = 2, friction = 0.01)
+        registerBodyWithFixture(type = BodyType.DYNAMIC, density = 2, friction = 1, fixedRotation = true)
         setupControls()
-        addFixedUpdater(30.timesPerSecond) {
-            position(pos.x + velocity.x, pos.y + velocity.y)
-        }
     }
 
     private fun setupControls() {
-        var dx = 0.0
-        var dy = 0.0
 
         keys {
-
+            up(Key.SPACE) {
+                body?.let { it.linearVelocityY += 30 }
+            }
         }
         addUpdaterWithViews { views: Views, dt: TimeSpan ->
-            val scale = dt.milliseconds / 1000
+            var dx = 0f
+            val scale = dt.milliseconds.toFloat() / 20
             with(views.input) {
                 when {
-                    keys[Key.RIGHT] -> dx += ACCELERATION * scale
-                    keys[Key.LEFT] -> dx -= ACCELERATION * scale
+                    keys[Key.RIGHT] -> dx = ACCELERATION * scale
+                    keys[Key.LEFT] -> dx = -ACCELERATION * scale
                 }
 
             }
-            velocity = Point(clamp(velocity.x + dx, -MAX_X_VEL, MAX_X_VEL), 0.0)
+            body?.let { it.linearVelocityX = clamp(it.linearVelocityX + dx, -MAX_X_VEL, MAX_X_VEL) }
         }
     }
 
--- src/commonMain/kotlin/ui/level/LevelView.kt
+++ src/commonMain/kotlin/ui/level/LevelView.kt
@@ -1,9 +1,12 @@
 package ui.level
 
+import com.soywiz.korge.box2d.registerBodyWithFixture
 import com.soywiz.korge.view.Container
 import com.soywiz.korge.view.position
 import com.soywiz.korge.view.solidRect
 import level.LevelMap
+import level.TileType
+import org.jbox2d.dynamics.BodyType
 
 const val TILE_SIZE = 10
 
@@ -20,5 +23,8 @@ private fun Container.paint(map: LevelMap, x: Int, y: Int) {
     val tile = map.getTile(x, y)
     solidRect(TILE_SIZE, TILE_SIZE, tile.type.color) {
         position(x * TILE_SIZE, y * TILE_SIZE)
+        if (tile.type == TileType.TILE) {
+            registerBodyWithFixture(type = BodyType.STATIC)
+        }
     }
 }
