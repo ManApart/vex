73c84f09ca50546c9de86f976d19a84b307638c9
ManApart <r@austinkucera.com>
2021-07-23 20:02:10 -0400

    smoother horizontal collision

--- src/commonMain/kotlin/ui/Player.kt
+++ src/commonMain/kotlin/ui/Player.kt
@@ -2,21 +2,17 @@ package ui
 
 import clamp
 import com.soywiz.klock.TimeSpan
-import com.soywiz.klock.timesPerSecond
 import com.soywiz.korev.Key
 import com.soywiz.korge.box2d.body
 import com.soywiz.korge.box2d.registerBodyWithFixture
 import com.soywiz.korge.input.keys
 import com.soywiz.korge.view.*
 import com.soywiz.korim.color.Colors
-import com.soywiz.korma.geom.Point
 import level.LevelMap
 import level.Tile
-import org.jbox2d.common.Vec2
+import org.jbox2d.dynamics.Body
 import org.jbox2d.dynamics.BodyType
 import ui.level.TILE_SIZE
-import kotlin.math.abs
-import kotlin.math.min
 
 const val MAX_X_VEL = 2f
 const val MAX_X_AIR_VEL = 6.0
@@ -35,20 +31,32 @@ private const val DASH_VELOCITY = 20
 private const val DASH_TIME = .15
 
 class Player(private val map: LevelMap) : Container() {
+    private lateinit var body: Body
 
     fun init(spawnTile: Tile) {
-        solidRect(0.9 * TILE_SIZE, 1.5 * TILE_SIZE, Colors.PINK)
-        position(spawnTile.x * TILE_SIZE, spawnTile.y * TILE_SIZE)
 
-        registerBodyWithFixture(type = BodyType.DYNAMIC, density = 2, friction = 1, fixedRotation = true)
-        setupControls()
+        val collider = ellipse(0.9 * TILE_SIZE / 2.0, 0.9 * TILE_SIZE / 2.0, Colors.PINK) {
+            alpha = 0.0
+            position(spawnTile.x * TILE_SIZE, spawnTile.y * TILE_SIZE)
+
+            registerBodyWithFixture(type = BodyType.DYNAMIC, density = 2, friction = 1, fixedRotation = true)
+            this@Player.body = body!!
+            setupControls()
+        }
+
+        solidRect(0.9 * TILE_SIZE, 1.5 * TILE_SIZE, Colors.PINK) {
+            addUpdater {
+                position(collider.x - TILE_SIZE / 2, collider.y - TILE_SIZE)
+            }
+        }
+
     }
 
     private fun setupControls() {
 
         keys {
-            up(Key.SPACE) {
-                body?.let { it.linearVelocityY += 30 }
+            justDown(Key.SPACE) {
+                body.let { it.linearVelocityY += 30 }
             }
         }
         addUpdaterWithViews { views: Views, dt: TimeSpan ->
