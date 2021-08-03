56ff60d6ed512b1a9a1a074c44f86728b5c14c8c
ManApart <r@austinkucera.com>
2021-08-03 08:02:17 -0400

    decent grapple collision

--- src/commonMain/kotlin/ui/level/GrapplingHook.kt
+++ src/commonMain/kotlin/ui/level/GrapplingHook.kt
@@ -1,40 +1,44 @@
 package ui.level
 
+import com.soywiz.korge.box2d.BoxShape
 import com.soywiz.korge.box2d.body
 import com.soywiz.korge.box2d.registerBodyWithFixture
 import com.soywiz.korge.view.*
 import com.soywiz.korim.color.Colors
 import com.soywiz.korma.geom.Angle
+import com.soywiz.korma.geom.Rectangle
 import com.soywiz.korma.geom.degrees
 import org.jbox2d.collision.shapes.CircleShape
 import org.jbox2d.dynamics.BodyType
 import toPoint
 import toVector
 
-private const val magnitude = 1.0f
+private const val magnitude = 10.0f
 
 class GrapplingHook(private val player: Player, angle: Angle) : Container() {
+    private var collided = false
+    private val rect: SolidRect
+
     init {
         println("Angle: ${angle.degrees}")
         player.parent?.addChild(this)
-        solidRect(TILE_SIZE, TILE_SIZE, Colors.RED)
+        rect = solidRect(TILE_SIZE, TILE_SIZE, Colors.RED)
         centerOn(player)
 
-        registerBodyWithFixture(
-            type = BodyType.DYNAMIC,
-            density = 2,
-            gravityScale = 0f,
-            fixedRotation = true,
-            shape = CircleShape(0.225),
-            restitution = 0,
-            bullet = true
-        )
         val initialVelocity = angle.toPoint().toVector()
         println("Velocity: $initialVelocity")
 
         addUpdater {
-            body!!._linearVelocity.x = initialVelocity.x * magnitude
-            body!!._linearVelocity.y = initialVelocity.y * magnitude
+            if (!collided) {
+                position(pos.x + initialVelocity.x * magnitude, pos.y - initialVelocity.y * magnitude)
+            }
+        }
+
+        onCollision({
+            it is SolidRect && it.body != null && it !is Player
+        }) {
+            collided = true
+            rect.color = Colors.GREEN
         }
     }
 
